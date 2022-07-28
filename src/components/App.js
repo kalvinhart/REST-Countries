f6eb3dce0b0d870c0d5615915a407039f3aad3ee
Kalvin <kalvinhart@hotmail.co.uk>
2022-07-28 14:58:57 +0100

    fix: amended broken api link

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import Header from "./Header";
 import Spinner from "./Spinner";
@@ -8,48 +8,66 @@ import CountryDetails from "./CountryDetails";
 import RouteError from "./RouteError";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            data: [],
-            loading: false
-        };
+    this.state = {
+      data: [],
+      loading: false,
+    };
 
-        this.getCountries = this.getCountries.bind(this);       
+    this.getCountries = this.getCountries.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.state.data.length < 1) {
+      this.setState({ loading: true }, this.getCountries);
     }
+  }
 
-    componentDidMount() {
-        if (this.state.data.length < 1) {
-            this.setState({loading: true}, this.getCountries);
-        }
+  async getCountries() {
+    const url = "https://restcountries.com/v2/all";
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      this.setState({ data: data, loading: false });
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    async getCountries() {
-        const url = "https://restcountries.eu/rest/v2/all";
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            this.setState({data: data, loading: false});
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <Header />
-                {this.state.loading ? <Spinner /> : 
-                    <Switch>
-                        <Route exact path="/" render={() => <SearchControls data={this.state.data} getCountries={this.getCountries}/>} /> 
-                        <Route exact path="/country/:name" render={(routeProps) => <CountryDetails data={this.state.data} getCountries={this.getCountries} {...routeProps}/>} />
-                        <Route component={RouteError} />
-                    </Switch>
-                }
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        {this.state.loading ? (
+          <Spinner />
+        ) : (
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <SearchControls data={this.state.data} getCountries={this.getCountries} />
+              )}
+            />
+            <Route
+              exact
+              path="/country/:name"
+              render={(routeProps) => (
+                <CountryDetails
+                  data={this.state.data}
+                  getCountries={this.getCountries}
+                  {...routeProps}
+                />
+              )}
+            />
+            <Route component={RouteError} />
+          </Switch>
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
